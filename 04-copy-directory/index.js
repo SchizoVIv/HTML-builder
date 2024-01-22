32150fefb5db306f531b10c92806be7b515390e3
Vlada Yakowleva <116439108+SchizoVIv@users.noreply.github.com>
2024-01-23 00:36:19 +0200

    feat: solve the task 04

---
+++ 04-copy-directory/index.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+
+// проверка на наличие папки и ее создание
+fs.readdir(path.join(__dirname), (err, files) => {
+  if (err) throw err;
+  if (files.includes('files-copy') === false) {
+    fs.mkdir(path.join(__dirname, 'files-copy'), (err) => {
+      if (err) throw err;
+    });
+  }
+});
+
+function copyFiles(el) {
+  const filePath = `./files/${el}`;
+  const filePathCopy = `./files-copy/${el}`;
+
+  fs.copyFile(filePath, filePathCopy, (err) => {
+    if (err) throw err;
+    console.log('Files copy successfully!');
+  });
+}
+
+fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+  if (err) throw err;
+  files.forEach((el) => {
+    copyFiles(el);
+  });
+});
