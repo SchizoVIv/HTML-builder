6554fb5c650f5c4bb119086a04ea2fa76d4829c7
Vlada Yakowleva <116439108+SchizoVIv@users.noreply.github.com>
2024-01-23 00:39:45 +0200

    feat: solve the task 05

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,24 @@
+let fs = require('fs');
+let path = require('path');
+let pathNewFile = path.join(__dirname, 'project-dist', 'bundle.css');
+let styles = path.join(__dirname, 'styles');
+
+fs.readdir(styles, 'utf-8', (err, files) => {
+  if (err) throw err;
+
+  fs.writeFile(pathNewFile, '', (err) => {
+    if (err) throw err;
+  });
+
+  files.forEach((el) => {
+    if (path.parse(path.join(styles, el)).ext === '.css') {
+      let stream = fs.createReadStream(path.join(styles, el));
+
+      stream.on('data', (data) => {
+        fs.appendFile(pathNewFile, data, (err) => {
+          if (err) throw err;
+        });
+      });
+    }
+  });
+});
