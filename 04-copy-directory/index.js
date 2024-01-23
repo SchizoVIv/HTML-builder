e2fa8d4267f13ddb188f84cb74f6d3ed641a6fa8
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-01-23 12:53:11 +0200

    fix: chenge path

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,6 +1,9 @@
 const fs = require('fs');
 const path = require('path');
 
+const pathDir = path.join(__dirname, 'files');
+const pathDirCopy = path.join(__dirname, 'files-copy');
+
 // проверка на наличие папки и ее создание
 fs.readdir(path.join(__dirname), (err, files) => {
   if (err) {
@@ -16,8 +19,8 @@ fs.readdir(path.join(__dirname), (err, files) => {
 });
 
 function copyFiles(el) {
-  const filePath = `./files/${el}`;
-  const filePathCopy = `./files-copy/${el}`;
+  const filePath = path.join(pathDir, el);
+  const filePathCopy = path.join(pathDirCopy, el);
 
   fs.copyFile(filePath, filePathCopy, (err) => {
     if (err) {
