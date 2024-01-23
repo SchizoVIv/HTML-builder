7f000dde4846b089f63c8335e2a362b13767da53
Vlada Yakowleva <116439108+SchizoVIv@users.noreply.github.com>
2024-01-23 14:26:51 +0200

    feat: add "Goodbye" before exit

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -1,20 +1,40 @@
-const { stdin, stdout, exit } = process;
 const fs = require('fs');
 const path = require('path');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
 const filePath = path.join(__dirname, 'text.txt');
 const writeStream = fs.createWriteStream(filePath, { flags: 'a' });
 
 function theEnd() {
-  stdout.write('Goodbye!');
-  exit();
+  rl.write('Goodbye!');
+  rl.pause();
+  process.exit();
 }
 
-stdout.write('Hello, please write a text...\n');
+rl.write('Please, write a text...\n');
 
-stdin.on('data', (data) => {
-  if (data.toString().toLowerCase().trim() === 'exit') {
-    theEnd();
-  }
-  writeStream.write(data);
+function writeText() {
+  rl.on('line', (answer) => {
+    if(answer.toLowerCase() === 'exit'){
+      return theEnd();
+    }
+    writeStream.write(answer);
+  });
+};
+
+writeText();
+
+rl.on('SIGINT', () => {
+  rl.question('Are you sure you want to exit? \n y(yes) to exit or else to continue \n ', (answer) => {
+    if (answer.match(/^y(es)?$/i)) { 
+      return theEnd();
+    } else {
+      writeText();
+    }
+  });
 });
