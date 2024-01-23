8264fdead080ccdbdf24c46858bb42a24a8fe520
Vlada Yakowleva <vladyakowleva@yandex.ru>
2024-01-23 17:06:41 +0200

    fix: remove reade .gitignore

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -10,16 +10,17 @@ fs.readdir(dirPath, (err, files) => {
   files.forEach((el) => {
     let filePath = path.join(dirPath, el);
     const name = path.parse((filePath)).name;
-    console.log(el);
-    fs.stat(filePath, (err, stats) => {
-      if (err) {
-        return console.log(err);
-      }
-      if (!stats.isDirectory()) {
-        const extName = path.extname(filePath).replace('.', '');
-        const size = stats.size;
-        console.log(`${name} - ${extName} - ${size}`);
-      }
-    });
+    if(el[0] !== '.'){
+      fs.stat(filePath, (err, stats) => {
+        if (err) {
+          return console.log(err);
+        }
+        if (!stats.isDirectory()) {
+          const extName = path.extname(filePath).replace('.', '');
+          const size = stats.size;
+          console.log(`${name} - ${extName} - ${size}`);
+        }
+      });
+    }
   });
 });
